a891c7c2345653a9f8139e3e7c7fe3b29b5ced87
Sean Young <sean@mess.org>
2023-04-20 08:51:29 +0100

    Add some EVM builtins
    Signed-off-by: Sean Young <sean@mess.org>

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1711,9 +1711,12 @@ pub enum Builtin {
     BlockHash,
     BlockNumber,
     Calldata,
+    ChainId,
     Gasleft,
     GasLimit,
     Gasprice,
+    BaseFee,
+    PrevRandao,
     GetAddress,
     ExtCodeSize,
     MinimumBalance,
@@ -1801,6 +1804,9 @@ impl From<&ast::Builtin> for Builtin {
             ast::Builtin::WriteUint128LE => Builtin::WriteUint128LE,
             ast::Builtin::WriteUint256LE => Builtin::WriteUint256LE,
             ast::Builtin::WriteBytes | ast::Builtin::WriteString => Builtin::WriteBytes,
+            ast::Builtin::ChainId => Builtin::ChainId,
+            ast::Builtin::BaseFee => Builtin::BaseFee,
+            ast::Builtin::PrevRandao => Builtin::PrevRandao,
             _ => panic!("Builtin should not be in the cfg"),
         }
     }
